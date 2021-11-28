2a6eedd67f75dd0374a2cdccb84c797d9a59a5a4
peresvetjke <igor.udalov@gmail.com>
2021-11-28 16:12:57 +0300

    Add matches filter

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
+gem 'bootstrap', '~> 5.1.3'
+gem 'jquery-rails'
+gem 'simple_form'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -11,8 +11,8 @@ class MatchesController < ApplicationController
   def find_matches
     @matches = Match.includes([:league, :home_team, :away_team]).all
     
-    if params[:rating].present?
-      @matches = @matches.top(params[:rating])
-    end
+    @matches = @matches.top(params[:rating]) if params[:rating].present?
+    @matches = @matches.where(league_id: params[:league_id]) if params[:league_id].present?
+    @matches = @matches.or(Match.matches_of_favorites_by_user(current_user)) if params[:include_favorites].present?
   end
 end
