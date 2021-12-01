1d71be6cc051d6abce76cda57f38d9db79ad99c5
peresvetjke <igor.udalov@gmail.com>
2021-12-01 15:24:33 +0300

    Add ajaj to matches index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,13 @@
 //= require jquery
 //= require popper
 //= require bootstrap-sprockets
+//= require underscore
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../../assets/javascripts/matches.js"
 
 Rails.start()
 Turbolinks.start()
